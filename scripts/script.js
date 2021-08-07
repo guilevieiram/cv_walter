f6a83380e2b1d17e381dfa1eaba14d4ec0cc462e
guilherme-vieira <guilhermevmanhaes@gmail.com>
2021-08-07 16:49:34 -0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,92 @@
+function importElement(elementId, elementSourceHTML){
+    $('#' + elementId).load(elementSourceHTML);
+}
+importElement('footer', 'components/footer.html');
+importElement('header', 'components/nav-bar.html');
+
+
+function load(element) {
+    console.log('loaded', element);
+    gsap.fromTo(
+        element,
+        {
+            opacity: 0,
+            y: 100
+        },
+        {
+            duration: 1.5,
+            opacity: 1,
+            y: 0,
+            ease: 'power1'
+        }
+    )
+};
+
+function loadOnReach (element) {
+    let target = document.querySelector(element);
+    var elementNotCalled = true;
+    gsap.set(element,{
+        opacity: 0,
+    });
+    window.addEventListener('scroll', () => {
+        console.log(window.scrollY, target.offsetTop)
+        if (window.scrollY + window.innerHeight > target.offsetTop && elementNotCalled){
+          elementNotCalled = false;
+          load(element)
+        }
+      });
+}
+
+
+loadOnReach('#professional')
+loadOnReach('#qualifications')
+loadOnReach('#academical')
+
+gsap.from("header", {
+    duration: 1.5, 
+    opacity: 0,
+    y: -50,
+    ease: 'power4'
+});
+
+gsap.from(".profile img",{
+    duration: 1.5,
+    x:-200,
+    opacity: 0,
+    ease: 'power1'
+});
+
+gsap.from(".profile-info h1",{
+    duration: 1,
+    y: -50,
+    opacity: 0,
+    ease: 'power1'
+});
+
+gsap.from(".profile-info p",{
+    duration: 1.5,
+    y: 100,
+    opacity: 0,
+    ease: 'power1'
+});
+
+gsap.from(".info",{
+    duration: 2,
+    y: 50,
+    opacity: 0.5,
+    ease: 'power1'
+});
+
+gsap.from(".contact-title",{
+    duration: 2,
+    y: 50,
+    opacity: 0.5,
+    ease: 'power1'
+});
+
+gsap.from(".contact-info",{
+    duration: 1,
+    y:200,
+    opacity: 0,
+    ease: 'power1'
+});
